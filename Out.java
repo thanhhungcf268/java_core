71940c710fcde8e7525daedebaf7c0e10a6de908
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-02 12:59:00 +0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ Out.java
@@ -0,0 +1,20 @@
+
+public class Out {
+
+    public static void main(String[] args) {
+        byte i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char a = 'a';
+        String s = "Hà Nội";
+        System.out.println("i =" + i);
+        System.out.println("f =" + f);
+        System.out.println("d =" + d);
+        System.out.println("b =" + b);
+        System.out.println("a =" + a);
+        System.out.println("s =" + s);
+
+    }
+
+}
