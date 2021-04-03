02d10592dd56797c88b3b0b0ab32ade4a9ee2a79
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-03 18:28:30 +0700

    [Bài tập] Hiển thị 20 số nguyên tố đầu tiên

---
+++ FindPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FindPrime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the prime number to find : ");
+        int choice = input.nextInt();
+        int check = 0;
+        int count = 0;
+        boolean check1 = true;
+        for (int i = 2; check1; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    check++;
+                }
+            }
+            if (check == 1) {
+                count++;
+                System.out.print(i + " ");
+                if (count == choice) {
+                    check1 = false;
+                }
+            }
+            check = 0;
+        }
+    }
+
+}
+
